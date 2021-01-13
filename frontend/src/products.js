d170208da40dd6a60a8bd86c9868ee147f617103
sr3devop <sr3devops@gmail.com>
2021-01-13 13:53:58 +0530

    Add frontend folder back to git repo

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+      _id: '1',
+      name: 'White Choco rasamalai',
+      image: '/images/pic1.jpg',
+      description:
+        'Made with rich white chocolate fude and milk solids ',
+      brand: 'KD',
+      category: 'White Chocolate',
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      _id: '2',
+      name: 'Rum & Raisin',
+      image: '/images/pic2.jpg',
+      description:
+        'Made with traditional Rum and Dark chocolate ',
+      brand: 'KD',
+      category: 'Dark Chocolate',
+      price: 599.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+    {
+      _id: '3',
+      name: 'ChocoNutey',
+      image: '/images/pic3.jpg',
+      description:
+      'Packed with Mixed nuts and coated with rich carmel',
+      brand: 'KD',
+      category: 'Nuts',
+      price: 929.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+    },
+    {
+      _id: '4',
+      name: 'Dark Chocolate',
+      image: '/images/pic4.jpg',
+      description:
+        'Experince the rich dark chocolate from Brazilian Coco',
+      brand: 'KD',
+      category: 'Dark Chocolates',
+      price: 399.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 12,
+    },
+    {
+      _id: '5',
+      name: 'Barfi',
+      image: '/images/pic5.jpg',
+      description:
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      brand: 'KD',
+      category: 'Fruit & Nuts',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+    },
+    {
+      _id: '6',
+      name: 'Kaju chocolatey',
+      image: '/images/pic7.jpg',
+      description:
+        'Enjoy the rich blend of Kaju and coco',
+      brand: 'KD',
+      category: 'Coco & Nuts',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 12,
+    },
+  ]
+  
+  export default products
